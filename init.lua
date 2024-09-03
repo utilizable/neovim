87821584390124070a3adcd40d8b0b6cbe0a8a29
Igor <33967454+utilizable@users.noreply.github.com>
2024-09-03 14:45:08 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,7 +2,14 @@
 -- CONFIG 
 --------------------------
 
+-- Control variable for initial setup
+local initial_setup = true  -- Change this to false to load all modules
+
+-- Require the plugins module
 require('plugins')
-require('remaps')
-require('set')
-require('nvim-tree')
+
+-- Conditionally require other modules based on the control variable
+if not initial_setup then
+    require('remaps')
+    require('set')
+end|
